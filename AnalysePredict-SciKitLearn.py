b7172f1ea62534d452d07ad5e382f17447b2b1bb
Nick Burch <nick@gagravarr.org>
2022-06-06 12:51:16 +0100

    Squares for guess scoring, and next steps

--- AnalysePredict-SciKitLearn.py
+++ AnalysePredict-SciKitLearn.py
@@ -38,6 +38,30 @@ from sklearn import metrics
 
 # ----------------------------------------------------------------------------
 
+result_green  = "\U0001F7E9"
+result_yellow = "\U0001F7E8"
+result_white  = "\u2B1C"
+result_black  = "\u25A0"
+
+# Prints squares for guesses like the real thing
+def calculate_squares(actual, guess):
+   res = []
+   for i in range (0,5):
+      if guess[i] == actual[i]:
+         res += result_green
+      elif guess[i] in actual:
+         res += result_yellow
+      else:
+         res += result_white
+   return "".join(res)
+
+print(calculate_squares("bbuzz","guess"))
+print(calculate_squares("bbuzz","uzbek"))
+print(calculate_squares("bbuzz","soyuz"))
+print(calculate_squares("bbuzz","bbuzz"))
+
+# ----------------------------------------------------------------------------
+
 # Read in the 5 letter words
 language = "british-english"
 words = pd.read_csv("wordle/%s"%language, header=0, names=["word"])
@@ -56,3 +80,36 @@ print(words.head(5))
 as_letters = words["word"].str.split('',n=5,expand=True).drop(0, axis=1)
 letter_counts = Counter(as_letters.values.flatten())
 print(letter_counts.most_common(10))
+
+# ----------------------------------------------------------------------------
+
+# What is a good starting word?
+# We want ones with as many popular letters as possible
+
+# If we include repeats?
+# TODO
+
+# Or if we want as many different popular letters as possible?
+# TODO
+
+# ----------------------------------------------------------------------------
+
+# Build a "scorer" for a given Word
+# Will return 6 scores for a given Word - Overall + Per-Letter
+# - Per-letter, give 1.0 if correct, 0.5 if wrong place, 0.0 if not used
+# - Overall, similar but with 0.8 for wrong place, and 0.2 if not used
+def score(answer, guess):
+   # TODO
+   return [0]
+
+# ----------------------------------------------------------------------------
+
+# TODO Something on TF-IDF
+# TODO Something on TF-DF which is more what we need
+
+# ----------------------------------------------------------------------------
+
+# Do we even need AI / ML?
+# Try with just....
+
+# ----------------------------------------------------------------------------
